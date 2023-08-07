58ea96da8f7663b3ad01caca0060f860f067b71c
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-08-08 00:37:36 +0100

    TDD

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
+"""defines function divide matrix"""
+
+
 def matrix_divided(matrix, div):
-    new_matrix = []
+    """divides matrix by scalar integer"""
+    import decimal
+    error_msg = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) is not list:
+        raise TypeError(error_msg)
+    len_rows = []
+    row_count = 0
     for row in matrix:
-        new_row = []
-        for elem in row:
-            new_element = round(elem / div, 2)
-            new_row.append(new_element)
-        new_matrix.append(new_row)
+        if type(row) is not list:
+            raise TypeError(error_msg)
+        len_rows.append(len(row))
+        for element in row:
+            if type(element) not in [int, float]:
+                raise TypeError(error_msg)
+        row_count += 1
+    if len(set(len_rows)) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if int(div) == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = list(map(lambda row:
+                          list(map(lambda x: round(x/div, 2), row)), matrix))
     return new_matrix
